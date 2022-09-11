9ccfde4015751ab56b4fd452f9c94639f1dd4ae5
seregakuvaev <seregakuvaev@mail.ru>
2022-09-11 13:57:48 +0300

    fu

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -8,6 +8,7 @@ import web.Service.CarService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Controller
 @RequestMapping()
@@ -20,15 +21,21 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/cars")
-    public String index(Model model) {
-        model.addAttribute("cars", carService.index());
-        return "index";
-    }
+//    @GetMapping("/cars")
+//    public String index(Model model) {
+//        model.addAttribute("cars", carService.index());
+//        return "index";
+//    }
+//
+//    @GetMapping(value = "/cars", params = "count")
+//    public String show(@RequestParam(value = "count", required = true) int count, Model model){
+//        model.addAttribute("cars", carService.show(count));
+//        return "show";
+//    }
 
-    @GetMapping(value = "/cars", params = "count")
-    public String show(@RequestParam(value = "count", required = true) int count, Model model){
-        model.addAttribute("cars", carService.show(count));
+    @GetMapping("/cars")
+    public String show(@RequestParam(value = "count", required = false) Integer count, Model model){
+        model.addAttribute("cars", carService.show(Objects.requireNonNullElse(count, 5)));
         return "show";
     }
 
